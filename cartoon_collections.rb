31629183f3f4cf1848d6acea9eb32b08732232c4
Ian Mayton <ian.mayton@gmail.com>
2020-07-18 14:51:45 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,8 +10,9 @@ def summon_captain_planet(planateer)
   end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+  calls.each do |call|
+    if call.length > 4
 end
 
 def find_the_cheese# code an argument here
