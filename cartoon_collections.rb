388c1205d0d443fcb37430f0da3db84c392c92ba
Ian Mayton <ian.mayton@gmail.com>
2020-07-18 14:57:36 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,7 +17,7 @@ end
 def find_the_cheese(strings)
   cheese_types = ["cheddar", "gouda", "camembert"]
   cheese_types.each do |cheese|
-    if strings.include(cheese)
+    if strings.include?(cheese)
       return cheese
     end
   end
